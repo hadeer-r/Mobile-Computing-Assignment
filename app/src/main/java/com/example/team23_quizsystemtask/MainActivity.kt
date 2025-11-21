1636bc26b77f6cf1abb9d6b1bc23131ca8d12fc2
Esraa Said <esraasaidmaray@gmail.com>
2025-11-21 15:09:08 +0200

    adding spinner code

--- app/src/main/java/com/example/team23_quizsystemtask/MainActivity.kt
+++ app/src/main/java/com/example/team23_quizsystemtask/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.team23_quizsystemtask.ui.theme.Team23QuizSystemTaskTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +26,14 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        val spinner = findViewById<Spinner>(R.id.idofspinner )
+        val categories = listOf("Geography", "Literature")
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            categories
+        )
+        spinner.adapter = adapter
     }
 }
 
